e2386da2be023e089df1978372eabfcf0d673206
davehob <hoblea@gmail.com>
2021-12-22 00:30:10 -0800

    sql adjust

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import re
 
 from flask import Flask
 from flask_restful import Api
@@ -11,13 +10,13 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-# rest of connection code using the connection string `uri`
+# uri = os.getenv("DATABASE_URL")  # or other relevant config var
+# if uri.startswith("postgres://"):
+#     uri = uri.replace("postgres://", "postgresql://", 1)
+# # rest of connection code using the connection string `uri`
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vkdlfdxdbyfsod:f6e6d3a75a2ca8ae5fee13dd2408b945e6ca9bcc0cfb1815e60edb6cdcfdbcdb@ec2-52-204-29-205.compute-1.amazonaws.com:5432/d8pnndod6kl51l'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
